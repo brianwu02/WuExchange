dc52821a7a28d368b3938de870b476ab0b57e798
Brian Wu <brianwu02@gmail.com>
2018-01-09 14:57:19 -0500

    add benchmarking script

---
+++ apps/wu_exchange_backend/benchmark.exs
@@ -0,0 +1,66 @@
+alias WuExchangeBackend.{
+  MatchingEngine,
+  Order
+}
+
+# do all setup required below
+{:ok, pid_1} = MatchingEngine.start_link()
+{:ok, pid_2} = MatchingEngine.start_link()
+{:ok, pid_3} = MatchingEngine.start_link()
+
+# setup MatchingEngine for a delete
+{:ok, pid_4} = MatchingEngine.start_link()
+
+
+Benchee.run(%{
+  "insert and delete a sell order": fn ->
+    {:ok, %Order{order_id: order_id_4}} = 
+      MatchingEngine.buy_limit_order(pid: pid_4, trader_id: 1, price_in_cents: 100, quantity: 100)
+    {:ok, _} = MatchingEngine.cancel_order(pid_4, order_id_4)
+  end,
+  # "delete a buy order from queue of size 100": fn ->
+  # {:ok, _} = MatchingEngine.cancel_order(pid_5, order_id_5)
+  # end,
+  "insert a buy limit order": fn -> 
+    MatchingEngine.buy_limit_order(pid: pid_1, trader_id: 1, price_in_cents: 100, quantity: 100)
+  end,
+  "insert a sell limit order": fn ->
+    MatchingEngine.sell_limit_order(pid: pid_1, trader_id: 1, price_in_cents: 100, quantity: 100)
+  end,
+  "insert 10 sell limit order": fn ->
+    for _ <- 0..10 do
+      MatchingEngine.sell_limit_order(pid: pid_1, trader_id: 1, price_in_cents: 100, quantity: 100)
+    end
+  end,
+  "insert 10 buy limit order": fn ->
+    for _ <- 0..10 do
+      MatchingEngine.buy_limit_order(pid: pid_2, trader_id: 1, price_in_cents: 100, quantity: 100)
+    end
+  end,
+  "insert 10 matching orders": fn ->
+    for _ <- 0..10 do
+      MatchingEngine.buy_limit_order(pid: pid_3, trader_id: 1, price_in_cents: 100, quantity: 100)
+      MatchingEngine.sell_limit_order(pid: pid_3, trader_id: 1, price_in_cents: 100, quantity: 100)
+    end
+  end,
+  "insert 1000 sell limit order": fn ->
+    for _ <- 0..1000 do
+      MatchingEngine.sell_limit_order(pid: pid_1, trader_id: 1, price_in_cents: 100, quantity: 100)
+    end
+  end,
+  "insert 1000 buy limit order": fn ->
+    for _ <- 0..1000 do
+      MatchingEngine.buy_limit_order(pid: pid_2, trader_id: 1, price_in_cents: 100, quantity: 100)
+    end
+  end,
+  "insert 1000 matching orders": fn ->
+    for _ <- 0..1000 do
+      MatchingEngine.buy_limit_order(pid: pid_3, trader_id: 1, price_in_cents: 100, quantity: 100)
+      MatchingEngine.sell_limit_order(pid: pid_3, trader_id: 1, price_in_cents: 100, quantity: 100)
+    end
+  end,
+},
+formatters: [
+  Benchee.Formatters.HTML
+  ]
+)
--- apps/wu_exchange_backend/mix.exs
+++ apps/wu_exchange_backend/mix.exs
@@ -26,6 +26,8 @@ defmodule WuExchangeBackend.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:benchee, "~> 0.11", only: :dev},
+      {:benchee_html, "~> 0.4", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
